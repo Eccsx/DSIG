183f79167b933652192c63f616857c06222c9661
Eccsx <thomasmattone83@outlook.fr>
2021-12-19 01:24:06 +0100

    :sparkles: neural network configurations

--- js/ocr.js
+++ js/ocr.js
@@ -7,7 +7,7 @@ const NUMBER_MATRIX_DIM = {
     'columns': 10
 };
 
-const NUMBER_TRAINING_SAMPLES = 2;
+const NUMBER_TRAINING_SAMPLES = 5;
 
 let numberMatrix = [];
 let trainingSet = [];
@@ -15,10 +15,51 @@ let trainingSet = [];
 let currentTrainingNumber = 0;
 let currentTrainingSamples = 1;
 
-const NN = new brain.NeuralNetwork();
+const NN_100 = new brain.NeuralNetwork({
+    activation: 'sigmoid',
+    hiddenLayers: [100]
+});
+const NN_50 = new brain.NeuralNetwork({
+    activation: 'sigmoid',
+    hiddenLayers: [50]
+});
+const NN_25 = new brain.NeuralNetwork({
+    activation: 'sigmoid',
+    hiddenLayers: [25]
+});
+const NN_100_50 = new brain.NeuralNetwork({
+    activation: 'sigmoid',
+    hiddenLayers: [100, 50]
+});
+const NN_100_25 = new brain.NeuralNetwork({
+    activation: 'sigmoid',
+    hiddenLayers: [100, 25]
+});
+const NN_50_25 = new brain.NeuralNetwork({
+    activation: 'sigmoid',
+    hiddenLayers: [50, 25]
+});
+const NN_50_15 = new brain.NeuralNetwork({
+    activation: 'sigmoid',
+    hiddenLayers: [50, 15]
+});
+const NN_100_50_25 = new brain.NeuralNetwork({
+    activation: 'sigmoid',
+    hiddenLayers: [100, 50, 25]
+});
 
-let prediction;
 let resultsCanvas, resultsChart;
+
+const CHART_COLORS = [
+    '#ffadad',
+    '#ffd6a5',
+    '#fdffb6',
+    '#caffbf',
+    '#9bf6ff',
+    '#a0c4ff',
+    '#bdb2ff',
+    '#ffc6ff'
+]
 
 /* ###################### */
 /* ### User Interface ### */
@@ -60,10 +101,6 @@ function setup() {
     resetMatrixButton.parent('left-container')
     resetMatrixButton.id('reset-button');
     resetMatrixButton.mousePressed(clearDrawingMatrix);
-
-
-
-
 
     initialization();
 }
@@ -202,7 +239,14 @@ function mouseDragged() {
 /* ###################### */
 
 function trainNN() {
-    NN.train(trainingSet);
+    NN_100.train(trainingSet);
+    NN_50.train(trainingSet);
+    NN_25.train(trainingSet);
+    NN_100_50.train(trainingSet);
+    NN_100_25.train(trainingSet);
+    NN_50_25.train(trainingSet);
+    NN_50_15.train(trainingSet);
+    NN_100_50_25.train(trainingSet);
 }
 
 function predict() {
@@ -210,10 +254,33 @@ function predict() {
     // https://stackoverflow.com/a/10865042/11060940
     const input = [].concat.apply([], numberMatrix);
 
-    prediction = NN.run(input);
+    // Run neural network configurations
+    let predictions = []
+    predictions.push(['{100}', NN_100.run(input)]);
+    predictions.push(['{50}', NN_50.run(input)]);
+    predictions.push(['{25}', NN_25.run(input)]);
+    predictions.push(['{100, 50}', NN_100_50.run(input)]);
+    predictions.push(['{100, 25}', NN_100_25.run(input)]);
+    predictions.push(['{50, 25}', NN_50_25.run(input)]);
+    predictions.push(['{50, 15}', NN_50_15.run(input)]);
+    predictions.push(['{100, 50, 25}', NN_100_50_25.run(input)]);
+
+    // Clear chart
+    resultsChart.data.datasets = [];
 
     // Update statistics plot
-    resultsChart.data.datasets[0].data = Object.values(prediction);
+    for (let i = 0; i < predictions.length; i++) {
+        // Create new dataset
+        const newDataset = {
+            label: predictions[i][0],
+            data: Object.values(predictions[i][1]),
+            borderWidth: 0,
+            backgroundColor: CHART_COLORS[i]
+        }
+
+        resultsChart.data.datasets.push(newDataset);
+    }
+
     resultsChart.update();
 }
 
@@ -242,19 +309,14 @@ function plotStatistics() {
     resultsChart = new Chart(resultsCanvas.getContext('2d'), {
         type: 'bar',
         data: {
-            labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-            datasets: [{
-                label: 'NN',
-                data: [],
-                backgroundColor: '#06d6a0',
-            }]
+            labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         },
         options: {
             responsive: false,
             plugins: {
                 title: {
                     display: true,
-                    text: 'NN results'
+                    text: 'Neural networks number matching'
                 }
             }
         }
