6c38abb9cd0f2c34b8a4b65d427ecf5fa4ee958d
Eccsx <thomasmattone83@outlook.fr>
2021-12-22 12:53:11 +0100

    :sparkles: sliders configurations

--- js/rosette.js
+++ js/rosette.js
@@ -2,8 +2,7 @@
 /* ### Global variables ### */
 /* ######################## */
 
-let rosetteSideLength = 50;
-let b1 = 24, b2 = 5, b3 = 18, b4 = 50;
+let gap = 3;
 
 /* ###################### */
 /* ### User Interface ### */
@@ -11,31 +10,79 @@ let b1 = 24, b2 = 5, b3 = 18, b4 = 50;
 
 const NAV_HEIGHT = 40;
 
+// Sliders
+let lengthSlider, petalSlider, b1Slider, b2Slider, b3Slider, b4Slider;
+
+// Text
+let lengthText, petalText, b1Text, b2Text, b3Text, b4Text;
+
 /* ####################### */
 /* ### P5.js functions ### */
 /* ####################### */
 
 function setup() {
     createCanvas(windowWidth, windowHeight - NAV_HEIGHT);
-    noLoop();
-    background(0);
+
+    // Setup sliders
+    petalSlider = createSlider(2, 10, 5, 1);
+    petalSlider.position(10, windowHeight - 130 - NAV_HEIGHT);
+    petalSlider.addClass('slider');
+
+    b1Slider = createSlider(0, 90, 0, 1);
+    b1Slider.position(10, windowHeight - 100 - NAV_HEIGHT);
+    b1Slider.addClass('slider');
+
+    b2Slider = createSlider(0, 90, 33, 1);
+    b2Slider.position(10, windowHeight - 70 - NAV_HEIGHT);
+    b2Slider.addClass('slider');
+
+    b3Slider = createSlider(0, 90, 15, 1);
+    b3Slider.position(10, windowHeight - 40 - NAV_HEIGHT);
+    b3Slider.addClass('slider');
+
+    b4Slider = createSlider(0, 90, 60, 1);
+    b4Slider.position(10, windowHeight - 10 - NAV_HEIGHT);
+    b4Slider.addClass('slider');
 }
 
 function draw() {
+    background(0);
+
+    // Sliders text
+    textSize(15);
+    fill(255);
+    textFont('Courier New');
+    textAlign(LEFT, CENTER);
+
+    petalText = text('p = ' + petalSlider.value(), 250, windowHeight - 160 - NAV_HEIGHT);
+    b1Text = text('b1 = ' + b1Slider.value(), 250, windowHeight - 130 - NAV_HEIGHT);
+    b2Text = text('b2 = ' + b2Slider.value(), 250, windowHeight - 100 - NAV_HEIGHT);
+    b3Text = text('b3 = ' + b3Slider.value(), 250, windowHeight - 70 - NAV_HEIGHT);
+    b4Text = text('b4 = ' + b4Slider.value(), 250, windowHeight - 40 - NAV_HEIGHT);
+
+    push();
+
+    // Tiling
     stroke(255);
     strokeWeight(1);
 
-    createTiling(rosetteSideLength, 6);
+    createTiling(
+        50,
+        petalSlider.value(),
+        b1Slider.value(), b2Slider.value(), b3Slider.value(), b4Slider.value()
+    );
+
+    pop();
 }
 
 /* ############## */
 /* ### Tiling ### */
 /* ############## */
 
-function createTiling(sideLength, rosettePetalNumber) {
+function createTiling(rosetteLength, rosettePetalNumber, b1, b2, b3, b4) {
     // Number of rosettes to fit canvas
-    const xTile = int(width / sideLength) + ( width % sideLength);
-    const yTile = int(height / sideLength) + ( height % sideLength);
+    const xTile = int(width / rosetteLength) + ( width % rosetteLength);
+    const yTile = int(height / rosetteLength) + ( height % rosetteLength);
 
     console.log(xTile, yTile)
 
@@ -44,11 +91,16 @@ function createTiling(sideLength, rosettePetalNumber) {
         for (let j = 0; j < yTile; j++) {
             // Rosette center
             const center = createVector(
-                i * sideLength * 2,
-                j * sideLength * 2
+                i * rosetteLength * gap,
+                j * rosetteLength * gap
             );
 
-            createRosette(sideLength, center, rosettePetalNumber);
+            createRosette(
+                rosetteLength,
+                center,
+                rosettePetalNumber,
+                b1, b2, b3, b4
+            );
         }
     }
 }
@@ -57,7 +109,7 @@ function createTiling(sideLength, rosettePetalNumber) {
 /* ### Rosette ### */
 /* ############### */
 
-function createRosette(sideLength, center, petalNumber) {
+function createRosette(sideLength, center, petalNumber, b1, b2, b3, b4) {
     // Petal rotation angle
     const theta = 2 * PI / petalNumber;
 
