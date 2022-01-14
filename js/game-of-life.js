0e6bf02d941e12c851c4c084e6135f6539b396fe
Eccsx <thomasmattone83@outlook.fr>
2022-01-14 22:15:26 +0100

    :sparkles: game of life rules

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -2,8 +2,8 @@
 /* ### Global variables ### */
 /* ######################## */
 
-const UNIVERSE_WIDTH = 10;
-const UNIVERSE_HEIGHT = 10;
+const UNIVERSE_WIDTH = 6;
+const UNIVERSE_HEIGHT = 6;
 
 let universe;
 
@@ -41,28 +41,9 @@ class Universe {
         // Create cells
         for (let y = 0; y < this.dimY; y++) {
             for (let x = 0; x < this.dimX; x++) {
-                this.cells.push(new Cell(x, y))
+                this.cells.push(new Cell(this, x, y, false))
             }
         }
-
-        // Assign cells neighbours
-        this.cells.forEach(cell => {
-            // Indices (Problem)
-            const leftX = (cell.x - 1 + this.dimX) % this.dimX;
-            const rightX = (cell.x + 1) % this.dimX;
-            const aboveY = (cell.y - 1 + this.dimY) % this.dimY;
-            const ddbelowY = (cell.y + 1) % this.dimY;
-
-            // Neighbours
-            cell.neighbourUp = this.getCell(cell.x, aboveY);
-            cell.neighbourUpRight = this.getCell(rightX, aboveY);
-            cell.neighbourRight = this.getCell(rightX, cell.y);
-            cell.neighbourDownRight = this.getCell(rightX, ddbelowY);
-            cell.neighbourDown = this.getCell(cell.x, ddbelowY);
-            cell.neighbourDownLeft= this.getCell(leftX, ddbelowY);
-            cell.neighbourLeft= this.getCell(leftX, cell.y);
-            cell.neighbourUpLeft= this.getCell(leftX, aboveY);
-        });
     }
 
     getCell(x, y) {
@@ -86,6 +67,40 @@ class Universe {
             }
         }
     }
+
+    nextGeneration() {
+        const nextGen = [];
+
+        // Apply game of life rules
+        // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules
+        this.cells.forEach(cell => {
+            // Current cell specs
+            const x = cell.x;
+            const y = cell.y;
+            const alive = this.getCell(x, y).isAlive;
+            const aliveNeighbours = this.getCell(x, y).getLiveNeighboursCount(this);
+
+            // Next generation cell state
+            let state;
+            if (alive) {
+                // Any live cell with two or three live neighbours survives
+                state = (aliveNeighbours == 2 ||aliveNeighbours == 3);
+            }
+            else {
+                // Any dead cell with three live neighbours becomes a live cell
+                state = (aliveNeighbours == 3)
+            }
+
+            // Create newt generation cell
+            nextGen.push(new Cell(this, x, y, state))
+        });
+
+        // Save next generation
+        this.cells = nextGen;
+
+        // Update universe
+        this.show();
+    }
 }
 
 /* ############ */
@@ -93,22 +108,38 @@ class Universe {
 /* ############ */
 
 class Cell {
-    constructor(x, y) {
+    constructor(universe, x, y, isAlive) {
+        this.universe = universe;
         this.x = x;
         this.y = y;
-        this.isAlive = false;
+        this.isAlive = isAlive;
+
+        // Assign neighbours indices
+        const leftX = (this.x - 1 + this.universe.dimX) % this.universe.dimX;
+        const rightX = (this.x + 1) % this.universe.dimX;
+        const aboveY = (this.y - 1 + this.universe.dimY) % this.universe.dimY;
+        const belowY = (this.y + 1) % this.universe.dimY;
+
+        this.IndexUp = [this.x, aboveY];
+        this.IndexUpRight = [rightX, aboveY];
+        this.IndexRight = [rightX, this.y];
+        this.IndexDownRight = [rightX, belowY];
+        this.IndexDown = [this.x, belowY];
+        this.IndexDownLeft = [leftX, belowY];
+        this.IndexLeft = [leftX, this.y];
+        this.IndexUpLeft = [leftX, aboveY];
     }
 
-    getNeighbours() {
+    getNeighboursState() {
         return [
-            this.neighbourUp,
-            this.neighbourUpRight,
-            this.neighbourRight,
-            this.neighbourDownRight,
-            this.neighbourDown,
-            this.neighbourDownLeft,
-            this.neighbourLeft,
-            this.neighbourUpLeft
+            this.universe.getCell(this.IndexUp[0], this.IndexUp[1]).isAlive,
+            this.universe.getCell(this.IndexUpRight[0], this.IndexUpRight[1]).isAlive,
+            this.universe.getCell(this.IndexRight[0], this.IndexRight[1]).isAlive,
+            this.universe.getCell(this.IndexDownRight[0], this.IndexDownRight[1]).isAlive,
+            this.universe.getCell(this.IndexDown[0], this.IndexDown[1]).isAlive,
+            this.universe.getCell(this.IndexDownLeft[0], this.IndexDownLeft[1]).isAlive,
+            this.universe.getCell(this.IndexLeft[0], this.IndexLeft[1]).isAlive,
+            this.universe.getCell(this.IndexUpLeft[0], this.IndexUpLeft[1]).isAlive
         ];
     }
 
@@ -118,6 +149,10 @@ class Cell {
 
     die() {
         this.isAlive = false;
+    }
+
+    getLiveNeighboursCount() {
+        return this.getNeighboursState().filter(Boolean).length;
     }
 }
 
@@ -132,7 +167,7 @@ function mousePressed() {
     const MATRIX_COLUMN_DIM = width / UNIVERSE_HEIGHT;
 
     // Check if mouse is over a cell
-    for (let y = 0; y <UNIVERSE_WIDTH; y++) {
+    for (let y = 0; y < UNIVERSE_WIDTH; y++) {
         for (let x = 0; x < UNIVERSE_HEIGHT; x++) {
             if (
                 mouseY > y * MATRIX_LINE_DIM &&
